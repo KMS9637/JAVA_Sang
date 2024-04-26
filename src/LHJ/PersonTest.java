1908e072d48ed8ba1b4161ba6fd0c34518034ee6
LeeHyunJong <vvlddiz@gmail.com>
2024-04-26 17:26:33 +0900

    테스트 추가됨.

---
+++ src/LHJ/PersonTest.java
@@ -0,0 +1,19 @@
+package LHJ;
+
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+		Person [] PersonTest = new Person[5];
+		
+		
+		PersonTest[0] = new Person("i");
+		PersonTest[1] = new Person("KMS");
+		PersonTest[2] = new Person("NGY");
+		PersonTest[3] = new Person("LSY");
+		PersonTest[4] = new Person("LYS");
+
+		
+	}
+
+}
