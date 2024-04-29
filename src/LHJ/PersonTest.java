27dc201a62dd55b9e389eb7744001c3da1a46104
LeeHyunJong <vvlddiz@gmail.com>
2024-04-29 14:42:52 +0900

    숙제 드디어 완성!

--- src/LHJ/PersonTest.java
+++ src/LHJ/PersonTest.java
@@ -1,25 +1,36 @@
 package LHJ;
 
 import KMS.KMS;
-import LeeYeonsu.Leeyeonsu_ExtendsPerson;
+//import LHJ.Person;
 import LSJ.LEESOOJIN;
+import LeeYeonsu.Leeyeonsu_ExtendsPerson;
 import NGY.NamGwiYeon;
 
 public class PersonTest {
 
-	public static void main(String[] args) {
-		Person [] PersonTest = new Person[5];
-		
-		
-		PersonTest[0] = new Person("i");
-		PersonTest[1] = new Person("KMS");
-		PersonTest[2] = new Person("NGY");
-		PersonTest[3] = new Person("LSJ");
-		PersonTest[4] = new Person("LeeYeonsu");
-
-		for (int i = 0; i < PersonTest.length; i++) {
-//			System.out.println("상속테스트 :" + PersonTest[i].walk());
-		}
-	}
-
+    public static void main(String[] args) {
+        Person[] personTest = new Person[5];
+        
+        personTest[0] = new Person("i");
+        personTest[1] = new KMS("KMS");
+        personTest[2] = new NamGwiYeon("NGY");
+        personTest[3] = new LEESOOJIN("LSJ");
+        personTest[4] = new Leeyeonsu_ExtendsPerson("LYS");
+        
+        for (Person person : personTest) {
+            if (person != null) {
+                System.out.println(person.getName() + "님이 ");
+                if (person instanceof KMS) {
+                    ((KMS) person).sleep();
+                } else if (person instanceof NamGwiYeon) {
+                    ((NamGwiYeon) person).walk();
+                } else if (person instanceof LEESOOJIN) {
+                    ((LEESOOJIN) person).walk();
+                } else if (person instanceof Leeyeonsu_ExtendsPerson) {
+                    ((Leeyeonsu_ExtendsPerson) person).speak();
+                }
+                System.out.println();
+            }
+        }
+    }
 }
